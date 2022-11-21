62fd6f246ee1fc47df80eccbacf992c2abd8c50a
Johan Sebastián Hernández <25942141+jseb2520@users.noreply.github.com>
2022-11-21 16:03:01 -0500

    Change schemas to match client's needs

--- src/api/service/controllers/service.js
+++ src/api/service/controllers/service.js
@@ -4,11 +4,40 @@
  * service controller
  */
 
+const request = require("request-promise");
 const { createCoreController } = require("@strapi/strapi").factories;
+
+const WA_PHONE_ID = process.env.WA_PHONE_NUMBER_ID;
+const WA_API_VERSION = process.env.WA_API_VERSION;
+
+const WABaseUrl = "https://graph.facebook.com";
 
 module.exports = createCoreController("api::service.service", ({ strapi }) => ({
   async create(ctx) {
     // some logic here
+    try {
+      await request({
+        method: "POST",
+        url: `${WABaseUrl}/${WA_API_VERSION}/${WA_PHONE_ID}/messages`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
+        },
+        body: {
+          messaging_product: "whatsapp",
+          to: "573114392798",
+          recipient_type: "individual",
+          type: "text",
+          text: {
+            preview_url: false,
+            body: "Esto es una prueba de la api de WA...",
+          },
+        },
+        json: true,
+      });
+    } catch (error) {
+      return [null, error];
+    }
     const response = await super.create(ctx);
     // some more logic
 
