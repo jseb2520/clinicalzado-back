599748ad19737abb29bd8d1e979a6f56ab42ae48
Johan Sebastián Hernández <25942141+jseb2520@users.noreply.github.com>
2022-11-24 13:21:41 -0500

    Add pg database

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 module.exports = {
   rest: {
-    defaultLimit: 100,
-    maxLimit: -1,
+    defaultLimit: 25,
+    maxLimit: 100,
     withCount: true,
   },
 };
--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "containers-us-west-101.railway.app"),
+      port: env.int("DATABASE_PORT", 7026),
+      database: env("DATABASE_NAME", "railway"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "8CLnXdwX7VkjHvl7Z72D"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    useNullAsDefault: true,
   },
 });
